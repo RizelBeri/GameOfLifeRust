2fd2ba09d4430d11619b7d7875a08989567b9ff9
RizelBeri <yevrostik@gmail.com>
2026-05-15 22:29:08 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,8 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use eframe::egui::{self, Color32, Panel, Pos2, Rect};
-use egui::containers::menu;
+use eframe::egui::{Color32, Panel, Pos2, Rect};
+use egui::{containers::menu, response};
 use std::time::{Duration, Instant};
 
 use crate::grid;
@@ -14,6 +14,7 @@ pub struct MyApp {
     gen_count: u64,
     last_tick: Instant,
     simulation_status: bool,
+    scene_rect: Rect,
 }
 
 impl MyApp {
@@ -30,21 +31,8 @@ impl MyApp {
             gen_count: 0,
             last_tick: Instant::now(),
             simulation_status: false,
+            scene_rect: Rect::ZERO,
         }
-    }
-}
-
-// draw new generation
-fn paint_grid(painter: &egui::Painter, grid: &grid::Grid) {
-    for (x, y) in &grid.cells {
-        painter.rect_filled(
-            Rect::from_center_size(
-                Pos2::new(*x as f32 * CELL_SIZE, *y as f32 * CELL_SIZE),
-                egui::Vec2::splat(CELL_SIZE),
-            ),
-            0.0,
-            Color32::WHITE,
-        );
     }
 }
 
@@ -89,8 +77,65 @@ impl eframe::App for MyApp {
         });
 
         egui::CentralPanel::default().show_inside(ui, |ui| {
-            let painter = ui.painter();
-            paint_grid(&painter, &self.grid);
+            egui::Frame::default().show(ui, |ui| {
+                let scene = egui::Scene::new().max_inner_size([350.0, 350.0]);
+
+                let response = scene.show(ui, &mut self.scene_rect, |ui| {
+                    let panel_rect = ui.max_rect();
+                    let painter = ui.painter_at(panel_rect);
+
+                    paint_gridlines(&painter, panel_rect.min, panel_rect.size());
+                    paint_grid(&painter, &self.grid, panel_rect.min);
+                });
+            });
+
+            // let panel_rect = ui.max_rect();
+            // let painter = ui.painter_at(panel_rect); // clips to panel automatically
         });
+    }
+}
+
+// draw new generation
+fn paint_grid(painter: &egui::Painter, grid: &grid::Grid, origin: egui::Pos2) {
+    for (x, y) in &grid.cells {
+        painter.rect_filled(
+            Rect::from_center_size(
+                Pos2::new(
+                    origin.x + *x as f32 * CELL_SIZE,
+                    origin.y + *y as f32 * CELL_SIZE,
+                ),
+                egui::Vec2::splat(CELL_SIZE),
+            ),
+            0.0,
+            Color32::DARK_GRAY,
+        );
+    }
+}
+
+fn paint_gridlines(painter: &egui::Painter, origin: egui::Pos2, screen: egui::Vec2) {
+    let stroke = egui::Stroke::new(0.5, Color32::DARK_GRAY);
+
+    // vertical lines
+    for x in 0..=(screen.x / CELL_SIZE) as i32 {
+        let x_pos = origin.x + x as f32 * CELL_SIZE;
+        painter.line_segment(
+            [
+                Pos2::new(x_pos, origin.y),
+                Pos2::new(x_pos, origin.y + screen.y),
+            ],
+            stroke,
+        );
+    }
+
+    // horizontal lines
+    for y in 0..=(screen.y / CELL_SIZE) as i32 {
+        let y_pos = origin.y + y as f32 * CELL_SIZE;
+        painter.line_segment(
+            [
+                Pos2::new(origin.x, y_pos),
+                Pos2::new(origin.x + screen.x, y_pos),
+            ],
+            stroke,
+        );
     }
 }
